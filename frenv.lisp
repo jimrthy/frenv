996c0a6844ea955b22d20dabee832db302fd8c49
James Ashley <james@teamdaves.us>
2013-04-25 21:01:17 -0500

    Start hacking

--- frenv.lisp
+++ frenv.lisp
@@ -2,5 +2,33 @@
 
 (in-package #:frenv)
 
-;;; "frenv" goes here. Hacks and glory await!
+(require '#:asdf)
+(asdf:oos 'asdf:load-op '#:cl-glfw)
+;; Fails because it tries to redefine a constant (+MAX_VARYING_COMPONENTS+)
+;(asdf:oos 'asdf:load-op '#:cl-glfw-opengl-version_3_3)
+;; Ditto
+;(asdf:oos 'asdf:load-op '#:cl-glfw-opengl-version_3_2)
+;; Ditto
+;(asdf:oos 'asdf:load-op '#:cl-glfw-opengl-version_3_1)
+
+;; I obviously need to get all those above errors fixed.
+(asdf:oos 'asdf:load-op '#:cl-glfw-opengl-version_2_1)
+
+;;; FIXME: Really do need to unify the various available
+;;; OpenGL versions to present a common API to the outside
+;;; world.
+
+;;; After all, that pretty much *is* the entire point to this
+;;; entire project.
+
+;;; Or, at least, it's really the first major milestone.
+
+;;; Set up a thread to listen on a socket...
+(error "Get this written")
+;; It seems highly probable that it will involve usocket and
+;; bordeaux-threads
+
+;;; Kick off the windowing system
+(error "Do that")
+
 
