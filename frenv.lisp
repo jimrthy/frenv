4f80e6ee9b5ecafab817b24da9b987e2943f3747
James Ashley <james@teamdaves.us>
2013-08-17 17:26:09 -0500

    Chugging along, getting a graphics thread set to run in the background. I think that resetting the lock around it is complete and total overkill, so that part's going away. Saving my place, just in case

--- frenv.lisp
+++ frenv.lisp
@@ -17,18 +17,14 @@
 ;; OTOH: shouldn't ASDF and packaging take care of this for me?
 (asdf:oos 'asdf:load-op '#:bordeaux-threads)
 
-;;; The top-level aspects of everything interesting.
-;;; From an MVC perspective, these represent both the models and the views.
-;;; It's important to remember that there could easily be many views
-;;; associated with any given model.
-;;; OTOH, it's a mistake to complect those concepts.
+;;; The top-level windows into everything interesting.
 ;; It's tempting to filter out the ones that are iconified,
 ;; but that isn't really fair. It's totally possible that they're generating stuff
 ;; that needs to be drawn (as painful as it is, think about the way things work in
 ;; windos 8).
 ;; Let the window manager do the filtering, until/unless it actually needs to be
 ;; optimized at this level.
-(defparameter *realities* ())
+(defparameter *views* ())
 
 (defgeneric render (renderer view)
   :documentation "Renderer generates a stream of OpenGL commands to represent view.")
@@ -43,7 +39,7 @@
   ;; draw. (Why isn't there? At least for power users?)
   ;; If nothing else, every alternative that comes to mind qualifies
   ;; as premature optimization.
-  (dolist (view (map 'list #'view *realities*))
+  (dolist (view *views*)
     ;; The view really doesn't know whether it's visible or not.
     (let ((renderer (driver view))
 	  (ctx (context view)))
@@ -51,45 +47,94 @@
       (glfw:make-context-current ctx)
       (render renderer view))))
 
+;;; The thing that any given view displays.
+;;; Could be a web page, a bingo card, a level in a FPS, a text file
+;;; to edit, or nothing at all if it makes more sense for something
+;;; else to take more control over the animation than what's offered
+;;; here.
+(defparameter *models* ())
+
+(defgeneric advance (model)
+  :documentation "Advance a model's state by one 'tick.'
+This could mean updating a rotation, advancing a lerp, switching
+a button's texture because of a mouse click, stepping a physics
+engine, or whatever.
+
+One point that seems important to keep in mind:
+User input is extremely important here. As is feedback from the
+server: maybe the screen should flash red because the player just
+got shot.")
+
 (defun animate ()
   ;; this entire idea is wrong.
   ;; There *should* be a timer to let various pieces know that it's time
   ;; to take a step.
   ;; User input provides the same sort of motivation.
   ;; There's no real justification for this sort of function at this level.
-  (error "Write this"))
+  ;; The fact that the implementation's ugly doesn't make the basic idea
+  ;; any more appealing.
+  ;; Oh well. I have to start somewhere.
+  (let ((altered-states nil))
+    ;; Note that this is most definitely *not* thread safe, but
+    ;; ...well, advance needs to be atomic.
+    ;; And having one model disappear while the others are
+    ;; being built would be a really bad idea.
+    ;; Hmm. Really need two lists: one active, the other
+    ;; inactive.
+    ;; Swap them at the start of this, then process the
+    ;; inactive, adding each result to the active.
+    ;; Any model that wants to go away can just return nil.
+    (dolist (model *models*)
+      (if-let ((new-state (advance model)))
+	;; I really want to use something like push here.
+	;; What am I missing?
+	(setf altered-states (cons new-state altered-states))))
+    (setf *models* altered-states)))
 
-(defun start ()
-  "Kick off the windowing system."
+(defun stupid-loop ()
+  "Really just because I have to start somewhere"
   ;; Why can't this be anonymous?
   (glfw:def-error-callback err-hand (msg)
     (format t "~A" msg))
-  (glfw:initialize))
+  (glfw:with-init
+    ;; This next part isn't even worth considering:
+    ;; I should really be basing this on either *views* or *models*
+    ;; Even that isn't correct. All of those could be gone, but the
+    ;; repl could still be ready and waiting to create a new
+    ;; window.
+    ;; But, this is a start.
+    (glfw:with-window (:title "Frenv" :width 800 :height 600
+			      :depthbits 16 :mode glfw:+window+)
+      (glfw:swap-interval 1)
 
-;; Cheap way for caller to wait on stupid-loop to exit.
-;; Seriously. The entire thing is dumb. It just all happens
-;; to qualify as "things I've managed to accomplish today"
-(defparameter *exited* ())
-(defun stupid-loop ()
-  "Really just because I have to start somewhere"
-  (glfw:with-window (:title "Frenv" :width 800 :height 600
-			    :depthbits 16 :mode glfw:+window+)
-    (glfw:swap-interval 1)
+      (unwind-protect
+	   ;; The until test is a complete and total failure,
+	   ;; the second I add a second window.
+	   (loop until (window-should-close-p)
+	      do (draw)
+	      do (animate)
+	      do (glfw:swap-buffers)
+	      do (poll-events))))))
 
-    (unwind-protect
-	 (loop until (window-should-close-p)
-	    do (draw)
-	    do (animate)
-	    do (glfw:swap-buffers)
-	    do (poll-events))
-      (setf *exited* t))))
+(defparameter *graphics-lock* nil) ; think of python's GIL: try to avoid at all costs.
+(defparameter *graphics-thread* nil)
+(defun start ()
+  "Kick off the windowing system."
+  (setf *graphics-lock* (bordeaux-threads:make-lock "Everybody's favorite renderer"))
+  ;; Run stupid-loop in a second thread.
+  (setf *graphics-thread* (bordeaux-threads:makethread stupid-loop :name "graphics")))
 
 (defun stop ()
-  (set-window-should-close)
-  ;; Really should be smarter about waiting for stupid-loop to exit
-  (loop until *exited*
-       (do (sleep 0.1)))
-  (glfw:terminate))
+  ;; Nope. Does not work at all.
+  ;; Well, might be OK for single-window setups.
+  (acquire-lock *graphics-lock* t)
+  (let ((lock *graphics-lock*))
+    (set-window-should-close)
+    (when *graphics-thread*
+      (bordeaux-threads:join-thread *graphics-thread*))
+    ;; Reset so start can rebuild...redundant, but I'm in a pedantic mood.
+    (setf *graphics-lock* nil)
+    (release-lock *graphics-lock*)))
 
 ;;; This next piece is absolutely horrible and should never
 ;;; see the light of day. It totally violates the entire principle
