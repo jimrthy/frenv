63aea730a33e0ce3de101fe79cb0db73265b3892
James Ashley <james@teamdaves.us>
2013-08-21 20:56:55 -0500

    Stupid compiler errors that I should have caught earlier

--- frenv.lisp
+++ frenv.lisp
@@ -2,16 +2,16 @@
 
 (in-package #:frenv)
 
-(require '#:asdf)
-;;(println "Get cl-glfw3 working and integrated")
-(asdf:oos 'asdf:load-op '#:cl-glfw3)
+;;(require '#:asdf)
+;;(format t "Get cl-glfw3 working and integrated")
+;;(asdf:oos 'asdf:load-op '#:cl-glfw3)
 
 ;; This next point's debatable.
 ;; I want to be able to spawn a window from a REPL and keep using the REPL.
 ;; That doesn't mean that everyone else also wants that (though I have
 ;; a tough time imagining why they wouldn't).
 ;; Really should just present this as a fait accompli.
-(println "Just accept that multiple windows really mean multiple threads")
+(format t "Just accept that multiple windows really mean multiple threads")
 ;; OTOH...they don't. Especially since rendering isn't thread-safe.
 ;; The glfw3 docs are full of functions that must be called from the main thread.
 ;; OTOH: shouldn't ASDF and packaging take care of this for me?
